b4ded40dd9fa6cf01b70248ba77c8f39d4e23ab6
Keijiro Takahashi <keijiro@gmail.com>
2018-01-04 19:49:03 +0900

    Added WireCube shader.

--- Assets/ShurikenPlus/Editor/AssetFactory.cs
+++ Assets/ShurikenPlus/Editor/AssetFactory.cs
@@ -18,6 +18,7 @@ namespace ShurikenPlus
         {
             AssetDatabase.CreateAsset(CreateNullTriangle(), "Assets/NullTriangle.asset");
             AssetDatabase.CreateAsset(CreateNullHexagon(), "Assets/NullHexagon.asset");
+            AssetDatabase.CreateAsset(CreateNullCube(), "Assets/NullCube.asset");
             AssetDatabase.SaveAssets();
         }
 
@@ -39,6 +40,73 @@ namespace ShurikenPlus
                     0, 1, 2,  0, 2, 3,
                     0, 3, 4,  0, 4, 5,
                     0, 5, 6,  0, 6, 1 }, 0, true);
+            mesh.UploadMeshData(true);
+            return mesh;
+        }
+
+        static Mesh CreateNullCube()
+        {
+
+            var mesh = new Mesh();
+
+            mesh.vertices = new Vector3[24];
+
+            {
+                var x = new Vector3(1, 0, 0);
+                var y = new Vector3(0, 1, 0);
+                var z = new Vector3(0, 0, 1);
+
+                mesh.normals = new Vector3[] {
+                    -x, -x, -x, -x,
+                     x,  x,  x,  x,
+                    -y, -y, -y, -y,
+                     y,  y,  y,  y,
+                    -z, -z, -z, -z,
+                     z,  z,  z,  z
+                };
+            }
+
+            {
+                var x = new Vector4(1, 0, 0, 1);
+                var y = new Vector4(0, 1, 0, 1);
+                var z = new Vector4(0, 0, 1, 1);
+
+                mesh.tangents = new Vector4[] {
+                    -y, -y, -y, -y,
+                     y,  y,  y,  y,
+                    -z, -z, -z, -z,
+                     z,  z,  z,  z,
+                    -x, -x, -x, -x,
+                     x,  x,  x,  x
+                };
+            }
+
+            {
+                var uv00 = new Vector2(0, 0);
+                var uv10 = new Vector2(1, 0);
+                var uv01 = new Vector2(0, 1);
+                var uv11 = new Vector2(1, 1);
+
+                mesh.uv = new Vector2[] {
+                    uv00, uv10, uv01, uv11,
+                    uv00, uv10, uv01, uv11,
+                    uv00, uv10, uv01, uv11,
+                    uv00, uv10, uv01, uv11,
+                    uv00, uv10, uv01, uv11,
+                    uv00, uv10, uv01, uv11
+                };
+            }
+
+            mesh.SetTriangles(
+                new int[] {
+                     0,  1,  2,   1,  3,  2,
+                     4,  5,  6,   5,  7,  6,
+                     8,  9, 10,   9, 11, 10,
+                    12, 13, 14,  13, 15, 14,
+                    16, 17, 18,  17, 19, 18,
+                    20, 21, 22,  21, 23, 22
+                }, 0, true);
+
             mesh.UploadMeshData(true);
             return mesh;
         }
---
+++ Assets/ShurikenPlus/Shaders/WireCube.shader
@@ -0,0 +1,91 @@
+﻿// ShurikenPlus - Custom shader library for Unity particle system
+// https://github.com/keijiro/ShurikenPlus
+
+// Procedurally randomized triangle mesh particle shader
+Shader "Shuriken Plus/Wire Cube"
+{
+    Properties
+    {
+        [HDR] _Color("Color", Color) = (0.5, 0.5, 0.5, 1)
+        [HDR] _EdgeColor("Edge Color", Color) = (1, 1, 1, 1)
+    }
+
+    CGINCLUDE
+
+    #include "Common.hlsl"
+
+    half4 _Color;
+    half4 _EdgeColor;
+
+    struct Varyings
+    {
+        float4 position : SV_POSITION;
+        half4 color : COLOR;
+        half2 edge : TEXCOORD0;
+        UNITY_FOG_COORDS(1)
+    };
+
+    Varyings Vertex(
+        float4 position : POSITION,
+        half3 normal : NORMAL,
+        half4 tangent : TANGENT,
+        half4 color : COLOR,
+        half4 texcoord0 : TEXCOORD0, // U, V, Rx, Ry
+        half4 texcoord1 : TEXCOORD1  // Rz, Sx, Sy, Sz
+    )
+    {
+        half3 bitangent = cross(normal, tangent.xyz);
+        half2 uv = (texcoord0.xy - 0.5) * 2;
+        half3 rot = half3(texcoord0.zw, texcoord1.x);
+        half3 size = texcoord1.yzw;
+
+        // Relative position of the vertex
+        float3 v = normal + tangent.xyz * uv.x + bitangent.xyz * uv.y;
+
+        // Particle rotation
+        v = mul(Euler3x3(rot / 360), v);
+
+        // Particle size
+        v *= size;
+
+        // Vertex output
+        Varyings output;
+        output.position = UnityObjectToClipPos(float4(position.xyz + v, 1));
+        output.color = color;
+        output.edge = uv;
+        UNITY_TRANSFER_FOG(output, output.position);
+        return output;
+    }
+
+    half4 Fragment(Varyings input) : SV_Target
+    {
+        // Edge detection
+        half2 bcc = 1 - abs(input.edge);
+        half2 edge2 = saturate(bcc / fwidth(bcc));
+        half edge = 1 - min(edge2.x, edge2.y);
+
+        // Color blending
+        half4 c = input.color * (_Color + _EdgeColor * edge);
+        c.a = saturate(c.a);
+
+        UNITY_APPLY_FOG(input.fogCoord, c);
+
+        return c;
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent" }
+        Cull off Blend SrcAlpha One ZWrite Off
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex Vertex
+            #pragma fragment Fragment
+            #pragma multi_compile_fog
+            ENDCG
+        }
+    }
+}
